a04a9df6fdc47086be2b611dccd1eab237a62122
wjl <2836596826@qq.com>
2018-11-23 10:37:26 +0800

    添加文件2

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -6,5 +6,6 @@ public class Main2 {
         System.out.println("2222222222222");
         System.out.println("33333333333333");
         System.out.println("444444444444");
+        System.out.println("444444444444");
     }
 }
